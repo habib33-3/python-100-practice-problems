7c9db04397d291ab6ebe7a7798e35b03cead7c59
Habibur Rahman <habib.swi16@gmail.com>
2023-08-05 00:14:41 +0600

    4. Write a program that will give you the sum of 3 digits

---
+++ 4_SumOfThreeDigit.py
@@ -0,0 +1,14 @@
+# 4. Write a program that will give you the sum of 3 digits
+#
+
+
+def calculate_sum(num1, num2, num3):
+    total = num1+num2+num3
+    print(total)
+
+
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+calculate_sum(n1, n2, n3)
